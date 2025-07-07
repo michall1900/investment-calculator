9a5530cccaea26a439fd9a282d3ac288d4677dd2
Michal Bucks <michall1900@gmail.com>
2025-07-07 11:35:26 +0300

    Add the header to the application while giving the header's template some html code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports:[Header]
 })
 export class AppComponent {}

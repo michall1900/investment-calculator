c59a0382e401184348d527988d421a986027f681
Michal Bucks <michall1900@gmail.com>
2025-07-07 12:43:21 +0300

    Edit the user-input template and put some html. Also the submit function added and some properties that should be given to other components later. Add also an interface of investment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { UserInput } from "./user-input/user-input";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports:[Header]
+  imports: [Header, UserInput]
 })
 export class AppComponent {}
--- src/app/user-input/user-input.ts
+++ src/app/user-input/user-input.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Investment } from '../investment.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.css'
 })
 export class UserInput {
+  
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+  
+
+  onSubmit(){
+    console.log("submited");
+    console.log(this.initialInvestment + this.annualInvestment, this.expectedReturn, this.duration);
+  }
+
 
 }

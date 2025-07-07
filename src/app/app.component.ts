9ca2f53d55c71e58b122346f8b02e1f3adcbcea0
Michal Bucks <michall1900@gmail.com>
2025-07-07 13:00:38 +0300

    Change app so it will catch the event and print it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { UserInput } from "./user-input/user-input";
+import { Investment } from './investment.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { UserInput } from "./user-input/user-input";
   templateUrl: './app.component.html',
   imports: [Header, UserInput]
 })
-export class AppComponent {}
+
+export class AppComponent {
+  onSubmitInvestment(investmentProperties: Investment){
+    console.log(investmentProperties);
+  }
+}
